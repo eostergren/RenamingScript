546b1bc42c2d00a1f1b09c82810bb23090aab892
eostergren <eostergren@live.com>
2016-09-03 23:41:55 -0400

    initial commit

---
+++ rename.rb
@@ -0,0 +1,30 @@
+all_files = Dir.entries('.')
+track_number = 0
+name_start_offset = 6
+song_name = String.new
+new_filename = String.new
+all_files.each { |file_list| 
+   if file_list.include? '.txt'
+      puts file_list + ":"
+      song_name_list = File.open file_list
+      song_name_list.each { |line|
+      track_id = line[/t-\d\d/]
+          if track_id != nil
+              song_name = line.slice(name_start_offset, line.size - name_start_offset)
+              song_name.chop!
+              all_files.each { |file_name|
+                  if file_name.include? track_id
+                      track_number+=1
+                      new_filename = sprintf( '%2d_%s.flac', track_number, song_name)
+                       if new_filename[0] == ' '
+                          new_filename[0] = '0'
+                       end 
+                       puts "rename  " + file_name + " to " + new_filename
+                  end
+              }
+           end
+      }
+   puts ""
+   end
+}
+

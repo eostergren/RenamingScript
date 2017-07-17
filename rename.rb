fda8323d1f51785c28fbe39e4739b92bcc8f1fb5
eostergren <eostergren@live.com>
2017-07-17 11:47:30 -0400

    complete operation, no dry run

--- rename.rb
+++ rename.rb
@@ -1,6 +1,6 @@
 all_files = Dir.entries('.')
 track_number = 0
-name_start_offset = 6
+name_start_offset = 7
 song_name = String.new
 new_filename = String.new
 all_files.each { |file_list| 
@@ -8,7 +8,7 @@ all_files.each { |file_list|
       puts file_list + ":"
       song_name_list = File.open file_list
       song_name_list.each { |line|
-      track_id = line[/t-\d\d/]
+      track_id = line[/[dD]\d[tT]\d\d/]
           if track_id != nil
               song_name = line.slice(name_start_offset, line.size - name_start_offset)
               song_name.chop!
@@ -20,6 +20,7 @@ all_files.each { |file_list|
                           new_filename[0] = '0'
                        end 
                        puts "rename  " + file_name + " to " + new_filename
+					   File.rename(file_name, new_filename)
                   end
               }
            end
